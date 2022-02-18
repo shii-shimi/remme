6630fb1003ca1dab3885c4fa5649fa6c8776bc29
shii-shimi <you@example.com>
2022-02-18 16:50:06 +0900

    全体のコーディング

---
+++ script.js
@@ -0,0 +1,117 @@
+$(function () {
+    const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+    const fade_move = 100; // どのぐらい要素を動かすか(px)
+    const fade_time = 2500; // フェードの時間(ms)
+    // フェード前のcssを定義
+    $(".comcept").css({
+      opacity: 0,
+      transform: "translateY(" + fade_move + "px)",
+      transition: fade_time + "ms",
+    });
+    // スクロールまたはロードするたびに実行
+    $(window).on("scroll load", function () {
+      const scroll_top = $(this).scrollTop();
+      const scroll_bottom = scroll_top + $(this).height();
+      const fade_position = scroll_bottom - fade_bottom;
+      $(".comcept").each(function () {
+        const this_position = $(this).offset().top;
+        if (fade_position > this_position) {
+          $(this).css({
+            opacity: 1,
+            transform: "translateY(0)",
+          });
+        }
+      });
+    });
+  });
+
+
+$(function () {
+    const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+    const fade_move = 100; // どのぐらい要素を動かすか(px)
+    const fade_time = 2500; // フェードの時間(ms)
+    // フェード前のcssを定義
+    $(".shop").css({
+      opacity: 0,
+      transform: "translateY(" + fade_move + "px)",
+      transition: fade_time + "ms",
+    });
+    // スクロールまたはロードするたびに実行
+    $(window).on("scroll load", function () {
+      const scroll_top = $(this).scrollTop();
+      const scroll_bottom = scroll_top + $(this).height();
+      const fade_position = scroll_bottom - fade_bottom;
+      $(".shop").each(function () {
+        const this_position = $(this).offset().top;
+        if (fade_position > this_position) {
+          $(this).css({
+            opacity: 1,
+            transform: "translateY(0)",
+          });
+        }
+      });
+    });
+});
+
+
+
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+    responsive: [
+      {
+      breakpoint: 769,//モニターの横幅が769px以下の見せ方
+      settings: {
+        slidesToShow: 2,//スライドを画面に2枚見せる
+        slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+      }
+    },
+    {
+      breakpoint: 426,//モニターの横幅が426px以下の見せ方
+      settings: {
+        slidesToShow: 1,//スライドを画面に1枚見せる
+        slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+      }
+    }
+  ]
+});
+
+
+
+
+
+ 
+
+
+  
+$(function () {
+    const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+    const fade_move = 100; // どのぐらい要素を動かすか(px)
+    const fade_time = 2500; // フェードの時間(ms)
+    // フェード前のcssを定義
+    $(".gifttop").css({
+      opacity: 0,
+      transform: "translateY(" + fade_move + "px)",
+      transition: fade_time + "ms",
+    });
+    // スクロールまたはロードするたびに実行
+    $(window).on("scroll load", function () {
+      const scroll_top = $(this).scrollTop();
+      const scroll_bottom = scroll_top + $(this).height();
+      const fade_position = scroll_bottom - fade_bottom;
+      $(".gifttop").each(function () {
+        const this_position = $(this).offset().top;
+        if (fade_position > this_position) {
+          $(this).css({
+            opacity: 1,
+            transform: "translateY(0)",
+          });
+        }
+      });
+    });
+});
