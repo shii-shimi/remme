f3d275c32cd92229b0ad76f11fb5afb4af5d5efb
shii-shimi <you@example.com>
2022-02-19 12:00:53 +0900

    レスポンシブ

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+      $('.menuclose').on('click', function(event) {
+        $('.hamburger').removeClass('active');
+        $('.globalMenuSp').removeClass('active');
+      });
+  });
+});
+
+
+
+
+
 $(function () {
     const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
     const fade_move = 100; // どのぐらい要素を動かすか(px)
@@ -24,6 +44,36 @@ $(function () {
       });
     });
   });
+
+
+
+  $(function () {
+    const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+    const fade_move = 100; // どのぐらい要素を動かすか(px)
+    const fade_time = 2500; // フェードの時間(ms)
+    // フェード前のcssを定義
+    $(".partition").css({
+      opacity: 0,
+      transform: "translateY(" + fade_move + "px)",
+      transition: fade_time + "ms",
+    });
+    // スクロールまたはロードするたびに実行
+    $(window).on("scroll load", function () {
+      const scroll_top = $(this).scrollTop();
+      const scroll_bottom = scroll_top + $(this).height();
+      const fade_position = scroll_bottom - fade_bottom;
+      $(".partition").each(function () {
+        const this_position = $(this).offset().top;
+        if (fade_position > this_position) {
+          $(this).css({
+            opacity: 1,
+            transform: "translateY(0)",
+          });
+        }
+      });
+    });
+  });
+
 
 
 $(function () {
@@ -55,6 +105,60 @@ $(function () {
 
 
 
+$(function () {
+  const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+  const fade_move = 100; // どのぐらい要素を動かすか(px)
+  const fade_time = 2500; // フェードの時間(ms)
+  // フェード前のcssを定義
+  $(".gifttop").css({
+    opacity: 0,
+    transform: "translateY(" + fade_move + "px)",
+    transition: fade_time + "ms",
+  });
+  // スクロールまたはロードするたびに実行
+  $(window).on("scroll load", function () {
+    const scroll_top = $(this).scrollTop();
+    const scroll_bottom = scroll_top + $(this).height();
+    const fade_position = scroll_bottom - fade_bottom;
+    $(".gifttop").each(function () {
+      const this_position = $(this).offset().top;
+      if (fade_position > this_position) {
+        $(this).css({
+          opacity: 1,
+          transform: "translateY(0)",
+        });
+      }
+    });
+  });
+});
+
+$(function () {
+const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+const fade_move = 100; // どのぐらい要素を動かすか(px)
+const fade_time = 2500; // フェードの時間(ms)
+// フェード前のcssを定義
+$(".base").css({
+  opacity: 0,
+  transform: "translateY(" + fade_move + "px)",
+  transition: fade_time + "ms",
+});
+// スクロールまたはロードするたびに実行
+$(window).on("scroll load", function () {
+  const scroll_top = $(this).scrollTop();
+  const scroll_bottom = scroll_top + $(this).height();
+  const fade_position = scroll_bottom - fade_bottom;
+  $(".base").each(function () {
+    const this_position = $(this).offset().top;
+    if (fade_position > this_position) {
+      $(this).css({
+        opacity: 1,
+        transform: "translateY(0)",
+      });
+    }
+  });
+});
+});
+
 $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
@@ -83,35 +187,4 @@ $('.slider').slick({
 
 
 
-
-
- 
-
-
   
-$(function () {
-    const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
-    const fade_move = 100; // どのぐらい要素を動かすか(px)
-    const fade_time = 2500; // フェードの時間(ms)
-    // フェード前のcssを定義
-    $(".gifttop").css({
-      opacity: 0,
-      transform: "translateY(" + fade_move + "px)",
-      transition: fade_time + "ms",
-    });
-    // スクロールまたはロードするたびに実行
-    $(window).on("scroll load", function () {
-      const scroll_top = $(this).scrollTop();
-      const scroll_bottom = scroll_top + $(this).height();
-      const fade_position = scroll_bottom - fade_bottom;
-      $(".gifttop").each(function () {
-        const this_position = $(this).offset().top;
-        if (fade_position > this_position) {
-          $(this).css({
-            opacity: 1,
-            transform: "translateY(0)",
-          });
-        }
-      });
-    });
-});
